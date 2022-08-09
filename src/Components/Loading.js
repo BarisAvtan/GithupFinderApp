6b05bd5528dc82f2068c45e553d6c5f5ba312c4c
Barış Avtan <baris.avtan@arvento.com>
2022-08-09 16:53:13 +0300

    GitHunFiinderV2

---
+++ src/Components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import loading from './loading.gif'
+import loading from '../Gif/cartoon-snail-loading.gif'
+const Loading = () => {
+    //$ npm install file-loader --save-dev  kurulmalı
+    //https://www.npmjs.com/package/file-loader
+    return (
+        <React.Fragment>
+            <img src={loading} alt="Loading..." style={{width: '200px', display: 'block', margin: 'auto'}}/>
+        </React.Fragment>
+    )
+}
+export  default Loading;
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Navbar extends Component {
+    render() {
+        return (
+           <nav className="navbar navbar-dark bg-primary">
+               <div className="container">
+                <a href="#" className="navbar-brand">
+                        <i className={this.props.icon}></i> {this.props.title}
+                </a>
+               </div>
+           </nav>
+        )
+    }
+}
+
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+//Bu şekilde de component içerisinde default props tanımlanabilir.
+// Navbar.defaultProps = {
+//     title :'Default GitHub'
+// }
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+export default Navbar
